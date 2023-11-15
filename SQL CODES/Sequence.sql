9e87bd711f88dd35e722221d29c1702f82e0958b
Alice Huynh <alicehuynh3105@gmail.com>
2023-11-15 12:52:46 -0700

    Add NOCACHE NOCYCLE to sequences

--- SQL CODES/Sequence.sql
+++ SQL CODES/Sequence.sql
@@ -1,22 +1,49 @@
--- Unsure of what value to start so I just made them all like this
 -- Made them all from the beginning so it is easy to locate
 -- Create sequence for BOOKS table
-CREATE SEQUENCE books_bookid_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE books_bookid_seq 
+    START WITH 1 
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
 
 -- Create sequence for AUTHORS table
-CREATE SEQUENCE authors_authorid_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE authors_authorid_seq 
+    START WITH 1 
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
 
 -- Create sequence for READERS table
-CREATE SEQUENCE readers_readerid_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE readers_readerid_seq 
+    START WITH 1 
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
 
 -- Create sequence for PUBLISHER table
-CREATE SEQUENCE publishers_pubid_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE publishers_pubid_seq 
+    START WITH 1 
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
 
 -- Create sequence for BOOK_COPY table
-CREATE SEQUENCE book_copy_bookid_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE book_copy_bookid_seq 
+    START WITH 1 
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
 
 -- Create sequence for HOLD table
-CREATE SEQUENCE hold_holdid_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE hold_holdid_seq 
+    START WITH 1 
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
 
 -- Create sequence for CHECKOUT table
-CREATE SEQUENCE checkout_checkoutid_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE checkout_checkoutid_seq 
+    START WITH 1 
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
