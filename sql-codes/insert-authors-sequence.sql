c7e9dd086d1d8e515e2111b58a84a8fea8e1b14b
ralphcristofer <rcpfugaban@gmail.com>
2023-11-15 15:31:44 -0500

    Updating filename

---
+++ sql-codes/insert-authors-sequence.sql
@@ -0,0 +1,13 @@
+-- Data inserts
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Eliana', 'Landry');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Amy', 'Snow');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Antwan', 'Arias');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Ana', 'Beltran');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Kathleen', 'Valencia');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Dylan', 'Huffman');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Raelynn', 'Russo');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Anaya', 'Solomon');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Leah', 'Wilkinson');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Blake', 'Mullen');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Ellias', 'Ellis');
+INSERT INTO authors (authorid, fname, lname) VALUES (authors_authorid_seq.NEXTVAL, 'Zoe', 'Ware');
