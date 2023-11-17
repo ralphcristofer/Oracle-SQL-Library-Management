8695b470c74cb55145033395dd2672200034fd2f
Alice Huynh <alicehuynh3105@gmail.com>
2023-11-17 11:22:29 -0700

    add data to book_copy table

--- sql-codes/00-full-library-system.sql
+++ sql-codes/00-full-library-system.sql
@@ -160,15 +160,31 @@ CREATE TABLE book_copy (
 );
 
 -- Insert data into the BOOK_COPY table
--- This is joining the info of books and publisher table while creating
--- book_copy unique ID
+-- Notice that there is repeated ISBN and even pubid but not the
+-- bookcopy_id. This is because the bookcopy_id is a primary key
 INSERT INTO book_copy (bookcopy_id, isbn, pubid)
-SELECT
-    book_copy_bookcopy_id_seq.nextval AS bookcopy_id,
-    b.isbn,
-    b.pubid
-FROM books b, publisher p
-WHERE b.pubid = p.pubid;
+VALUES(book_copy_bookcopy_id_seq.nextval,5111129073, 1);
+
+INSERT INTO book_copy (bookcopy_id, isbn, pubid)
+VALUES(book_copy_bookcopy_id_seq.nextval,5665170631, 7);
+
+INSERT INTO book_copy (bookcopy_id, isbn, pubid)
+VALUES(book_copy_bookcopy_id_seq.nextval,5665170631, 7);
+
+INSERT INTO book_copy (bookcopy_id, isbn, pubid)
+VALUES(book_copy_bookcopy_id_seq.nextval,5941091990, 11);
+
+INSERT INTO book_copy (bookcopy_id, isbn, pubid)
+VALUES(book_copy_bookcopy_id_seq.nextval,5941091990, 11);
+
+INSERT INTO book_copy (bookcopy_id, isbn, pubid)
+VALUES(book_copy_bookcopy_id_seq.nextval,3649138913, 8);
+
+INSERT INTO book_copy (bookcopy_id, isbn, pubid)
+VALUES(book_copy_bookcopy_id_seq.nextval,3508689646, 6);
+
+INSERT INTO book_copy (bookcopy_id, isbn, pubid)
+VALUES(book_copy_bookcopy_id_seq.nextval,3508689646, 6);
 
 -- Create index for BOOK_COPY table
 CREATE INDEX idx_book_copy_isbn ON book_copy(isbn);
